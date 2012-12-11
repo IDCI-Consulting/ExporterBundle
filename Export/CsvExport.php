ba68b4ebdf2c13ebe885075da5392f492b9e589c
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2012-12-11 18:25:02 +0100

    Override addContent for csv exporter to get entity data on one row end by a EOL

--- Export/CsvExport.php
+++ Export/CsvExport.php
@@ -5,4 +5,15 @@ namespace IDCI\Bundle\ExporterBundle\Export;
 class CsvExport extends AbstractExport
 {
     protected $contentType = 'text/csv';
+
+    /**
+     * addContent
+     *
+     * @param string $content
+     */
+    public function addContent($content)
+    {
+        $content = trim( preg_replace( '/\s+/', ' ', $content ) );
+        parent::addContent($content.PHP_EOL);
+    }
 }
