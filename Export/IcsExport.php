b4695b70b99996dc6796aa41252481e469d8620e
Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
2012-12-12 12:06:54 +0100

    Add build header and footer for the icalendar format (ics)

--- Export/IcsExport.php
+++ Export/IcsExport.php
@@ -5,4 +5,17 @@ namespace IDCI\Bundle\ExporterBundle\Export;
 class IcsExport extends AbstractExport
 {
     protected $contentType = 'text/ics';
+    
+    public function buildHeader()
+    {
+        $this->addContent('BEGIN:VCALENDAR'.PHP_EOL);
+        $this->addContent('VERSION:2.0'.PHP_EOL);
+        $this->addContent('PRODID:-//hacksw/handcal//NONSGML v1.0//EN'.PHP_EOL.PHP_EOL);
+    }
+
+    public function buildFooter()
+    {
+        $this->addContent('END:VCALENDAR');
+    }
 }
+
