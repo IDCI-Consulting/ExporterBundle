IDCI-Consulting/ExporterBundle

commit 162c8c7c0af0eb9bf254499bd9c8b68bd8da2e69

Rename variables which do not respect symfony standards

Controller/ApiController.php
@@ -26,19 +26,19 @@ class ApiController extends Controller
      * @Route("/query", name="exporter_api_norewrite", defaults={"entity_reference"="null", "_format"="xml"})
      * @Route("/{entity_reference}.{_format}", name="exporter_api", defaults={"_format"="xml"})
      */
-    public function indexAction(Request $request, $entity_reference)
+    public function indexAction(Request $request, $entityReference)
     {
         $format = $request->getRequestFormat();
         if($request->query->has('format')) {
             $format = $request->query->get('format');
         }
 
-        if($entity_reference == "null") {
-            $entity_reference = $request->query->get('entity_reference');
+        if($entityReference == "null") {
+            $entityReference = $request->query->get('entity_reference');
         }
 
         $entities = $this->get('idci_exporter.manager')->extract(
-            $entity_reference,
+            $entityReference,
             $request->query->all()
         );
 

Service/Manager.php
@@ -29,15 +29,15 @@ class Manager
     /**
      * getEntityReferenceConfiguration
      *
-     * @param string $entity_reference
+     * @param string $entityReference
      * @return array
      * @throw UndefinedExportableEntityException
      */
-    public function getEntityReferenceConfiguration($entity_reference)
+    public function getEntityReferenceConfiguration($entityReference)
     {
         $configuration = $this->container->getParameter('exporterConfiguration');
-        if(isset($configuration['entities'][$entity_reference])) {
-            return $configuration['entities'][$entity_reference];
+        if(isset($configuration['entities'][$entityReference])) {
+            return $configuration['entities'][$entityReference];
         }
 
         throw new UndefinedExportableEntityException();
@@ -102,12 +102,12 @@ class Manager
     /**
      * guessEntityRepository
      *
-     * @param string $entity_reference
+     * @param string $entityReference
      * @return Repository
      */
-    public function guessEntityRepository($entity_reference)
+    public function guessEntityRepository($entityReference)
     {
-        $entityConfiguration = $this->getEntityReferenceConfiguration($entity_reference);
+        $entityConfiguration = $this->getEntityReferenceConfiguration($entityReference);
         $class = $entityConfiguration['class'];
         $em = $this->getEntityManager();
 
@@ -144,13 +144,13 @@ class Manager
     /**
      * extract
      *
-     * @param string $entity_reference
+     * @param string $entityReference
      * @param array $params
      * @return DoctrineCollection
      */
-    public function extract($entity_reference, $params = array())
+    public function extract($entityReference, $params = array())
     {
-        $repository = $this->guessEntityRepository($entity_reference);
+        $repository = $this->guessEntityRepository($entityReference);
 
         $reflectionClass = $this->getEntityReflectionClass($repository);
         if($reflectionClass->hasMethod('extract')) {

commit 6498b16cf1f0a5b0dc0a7ba73768eef1e59a5f85

Correct matching parameters in exporter api route
Bug introduced by the commit 162c8c7c0af0eb9bf254499bd9c8b68bd8da2e69.

Controller/ApiController.php
@@ -23,8 +23,8 @@ use Symfony\Component\HttpFoundation\Response;
 class ApiController extends Controller
 {
     /**
-     * @Route("/query", name="exporter_api_norewrite", defaults={"entity_reference"="null", "_format"="xml"})
-     * @Route("/{entity_reference}.{_format}", name="exporter_api", defaults={"_format"="xml"})
+     * @Route("/query", name="exporter_api_norewrite", defaults={"entityReference"="null", "_format"="xml"})
+     * @Route("/{entityReference}.{_format}", name="exporter_api", defaults={"_format"="xml"})
      */
     public function indexAction(Request $request, $entityReference)
     {
@@ -34,7 +34,7 @@ class ApiController extends Controller
         }
 
         if($entityReference == "null") {
-            $entityReference = $request->query->get('entity_reference');
+            $entityReference = $request->query->get('entityReference');
         }
 
         $entities = $this->get('idci_exporter.manager')->extract(